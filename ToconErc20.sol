0c97f27453f2fa2632514d3d372b473b4c302804
cmatan10 <cmatan10@gmail.com>
2022-04-25 15:02:56 +0300

    change cntract name

--- ToconErc20.sol
+++ ToconErc20.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract N2DRewards is ERC20, ERC20Burnable, Ownable {
+contract ToconErc20 is ERC20, ERC20Burnable, Ownable {
 
   mapping(address => bool) controllers;
   
